49b95a13bfafeab91aeb878d0b61e3d461fe976d
Ivan Fishchenko <fishchenko01@yandex.ru>
2023-12-29 20:25:23 +0300

    Сделал 1 версию проекта

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,43 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template').content;
+
 // @todo: DOM узлы
+
 
 // @todo: Функция создания карточки
 
+initialCards.forEach(function(element) {
+  addCard(element);
+  return element;
+});
+
+function addCard(element, DeleteCards) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const placesList = document.querySelector('.places__list');
+  cardElement.querySelector('.card__image').src = element.link;
+  cardElement.querySelector('.card__title').textContent = element.name;
+
+  placesList.append(cardElement);
+
+  const cardDeletebutton = cardElement.querySelector('.card__delete-button');
+  cardDeletebutton.addEventListener('click', function () {cardElement.remove()});
+}
+
 // @todo: Функция удаления карточки
+
+
+// Удаляется только первая карточка, если использовать такой способ 
+// function DeleteCards(evt) {
+//   let listItem = evt.target.closest('.places__item');
+//   listItem.remove();
+//   // listItem.remove();
+  
+// }
+
+// cardDeletebutton.addEventListener('click', function(evt) {
+//   DeleteCards(evt);
+// });
 
 // @todo: Вывести карточки на страницу
