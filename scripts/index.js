f46c9a644ae552d1491dc905f24b6660c7cf2552
Ivan Fishchenko <fishchenko01@yandex.ru>
2023-12-30 17:08:11 +0300

    Сделал 2 версию проекта

--- scripts/index.js
+++ scripts/index.js
@@ -4,40 +4,35 @@ const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
 
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
-initialCards.forEach(function(element) {
-  addCard(element);
-  return element;
+initialCards.forEach((item) => {
+  addCard(item);
+  return item;
 });
 
-function addCard(element, DeleteCards) {
+function createCard(item, deleteCard) {
   const cardTemplate = document.querySelector('#card-template').content;
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-  const placesList = document.querySelector('.places__list');
-  cardElement.querySelector('.card__image').src = element.link;
-  cardElement.querySelector('.card__title').textContent = element.name;
-
-  placesList.append(cardElement);
-
+  cardElement.querySelector('.card__image').src = item.link;
+  cardElement.querySelector('.card__image').alt = item.name;
+  cardElement.querySelector('.card__title').textContent = item.name;
   const cardDeletebutton = cardElement.querySelector('.card__delete-button');
-  cardDeletebutton.addEventListener('click', function () {cardElement.remove()});
+  cardDeletebutton.addEventListener('click', (e) => {deleteCard(e)});
+  return cardElement;
 }
 
 // @todo: Функция удаления карточки
 
-
-// Удаляется только первая карточка, если использовать такой способ 
-// function DeleteCards(evt) {
-//   let listItem = evt.target.closest('.places__item');
-//   listItem.remove();
-//   // listItem.remove();
-  
-// }
-
-// cardDeletebutton.addEventListener('click', function(evt) {
-//   DeleteCards(evt);
-// });
+function deleteCard(e) {
+  e.target.closest('.places__item').remove()
+}
 
 // @todo: Вывести карточки на страницу
+
+function addCard(item) {
+  const cardElement = createCard(item,deleteCard);
+  placesList.append(cardElement);
+}
